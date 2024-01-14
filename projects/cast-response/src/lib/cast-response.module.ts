eee98fc4d843a0cd2f034a91743284ae09a53d9b
BeSaRa <ahmedbesara@gmailcom>
2024-01-14 22:20:09 +0300

    feat: provideInterceptors function to use it with standalone component style

--- projects/cast-response/src/lib/cast-response.module.ts
+++ projects/cast-response/src/lib/cast-response.module.ts
@@ -1,4 +1,9 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import {
+  EnvironmentProviders,
+  makeEnvironmentProviders,
+  ModuleWithProviders,
+  NgModule,
+} from '@angular/core';
 import { GeneralInterceptor } from './general-interceptor';
 import { ClassConstructor } from './types/constructors';
 import { GeneralInterceptorContract } from './contracts/general-interceptor-contract';
@@ -34,11 +39,21 @@ export class CastResponseModule {
   }
 }
 
-// export function provideInterceptors(
-//   interceptors: ClassConstructor<GeneralInterceptorContract>[]
-// ) {
-//   GeneralInterceptor.interceptors = GeneralInterceptor.interceptors.concat(
-//     interceptors.map((interceptor) => new interceptor())
-//   );
-//   return;
-// }
+/**
+ * @description provide all general interceptor that you need any model has @InterceptModel or @CastResponse to pass through it
+ * @param interceptors
+ */
+export function provideInterceptors(
+  interceptors?: ClassConstructor<GeneralInterceptorContract>[]
+): EnvironmentProviders {
+  interceptors &&
+    (GeneralInterceptor.interceptors = GeneralInterceptor.interceptors.concat(
+      interceptors.map((interceptor) => new interceptor())
+    ));
+  return makeEnvironmentProviders([
+    {
+      provide: GeneralInterceptor,
+      useValue: GeneralInterceptor.interceptors,
+    },
+  ]);
+}
